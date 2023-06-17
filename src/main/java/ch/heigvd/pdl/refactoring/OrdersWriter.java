178ab3d50e06009dc5757a389470514593fa05d1
DrC0okie <timothee.vanhove@heig-vd.ch>
2023-06-17 14:15:44 +0200

    Refactored the whole OrdersWriter with an actual JSONObject package
    This class creates a JSON object manually, which is not ideal. So we use the org.json library to create JSON objects, which simplifies the construction of the JSON string and makes the code cleaner and easier to read. We also have broken down the getContents method into smaller, more manageable methods (createOrderObject and createProductObject) that each handle a specific part of the JSON string creation. This makes the code easier to understand and maintain, addressing the "Long Method" code smell.

--- src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
+++ src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
@@ -1,92 +1,54 @@
 package ch.heigvd.pdl.refactoring;
 
-public class OrdersWriter {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-    private Orders orders;
+public class OrdersWriter {
+    private final Orders orders;
 
     public OrdersWriter(Orders orders) {
         this.orders = orders;
     }
 
     public String getContents() {
-        StringBuffer sb = new StringBuffer("{\"orders\": [");
+        JSONObject jsonObject = new JSONObject();
+        JSONArray ordersArray = new JSONArray();
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
-            Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-
-                sb.append("{");
-                sb.append("\"code\": \"");
-                sb.append(product.getCode());
-                sb.append("\", ");
-                sb.append("\"color\": \"");
-                sb.append(getColorFor(product));
-                sb.append("\", ");
-
-                if (product.getSize() != Product.SIZE_NOT_APPLICABLE) {
-                    sb.append("\"size\": \"");
-                    sb.append(getSizeFor(product));
-                    sb.append("\", ");
-                }
-
-                sb.append("\"price\": ");
-                sb.append(product.getPrice());
-                sb.append(", ");
-                sb.append("\"currency\": \"");
-                sb.append(product.getCurrency());
-                sb.append("\"}, ");
-            }
-
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
-            sb.append("]");
-            sb.append("}, ");
+            ordersArray.put(createOrderObject(orders.getOrder(i)));
         }
 
-        if (orders.getOrdersCount() > 0) {
-            sb.delete(sb.length() - 2, sb.length());
-        }
-
-        return sb.append("]}").toString();
+        jsonObject.put("orders", ordersArray);
+        return jsonObject.toString();
     }
 
-    private String getSizeFor(Product product) {
-        switch (product.getSize()) {
-            case 1:
-                return "XS";
-            case 2:
-                return "S";
-            case 3:
-                return "M";
-            case 4:
-                return "L";
-            case 5:
-                return "XL";
-            case 6:
-                return "XXL";
-            default:
-                return "Invalid Size";
+    private JSONObject createOrderObject(Order order) {
+        JSONObject orderObject = new JSONObject();
+        JSONArray productsArray = new JSONArray();
+
+        for (Product product : order.getProducts()) {
+            productsArray.put(createProductObject(product));
         }
+
+        orderObject.put("id", order.getOrderId());
+        orderObject.put("products", productsArray);
+
+        return orderObject;
     }
 
-    private String getColorFor(Product product) {
-        switch (product.getColor()) {
-            case 1:
-                return "blue";
-            case 2:
-                return "red";
-            case 3:
-                return "yellow";
-            default:
-                return "no color";
+    private JSONObject createProductObject(Product product) {
+        JSONObject productObject = new JSONObject();
+
+        productObject.put("code", product.getCode());
+        productObject.put("color", product.getColor());
+
+        if (product.getSize() != null) {
+            productObject.put("size", product.getSize());
         }
+
+        productObject.put("price", product.getPrice());
+        productObject.put("currency", product.getCurrency());
+
+        return productObject;
     }
 }
